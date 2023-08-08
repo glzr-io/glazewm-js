dbbeca88e5627534eb815e2f0524ca6064a8a646
Lars Berger <lars.berger@yahoo.com>
2023-08-09 01:15:32 +0800

    feat: use `globalThis` for getting `Websocket` api in browsers

--- src/websocket.ts
+++ src/websocket.ts
@@ -4,4 +4,4 @@ import WebSocket from 'ws';
  * Instance of `Websocket` to use. Uses the `Websocket` web API when running in
  * the browser, otherwise uses `ws` when running Node.
  */
-export default global?.WebSocket ?? window?.WebSocket ?? WebSocket;
+export default globalThis.WebSocket ?? WebSocket;
