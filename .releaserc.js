6ed9e6a3ea03a413ce0ae50d7e06b048e5f2c21b
Lars Berger <lars.berger@yahoo.com>
2023-08-30 23:59:47 +0800

    chore: add semantic release config

---
+++ .releaserc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  branches: [
+    { name: 'main' },
+    { name: 'develop', channel: 'next', prerelease: true },
+  ],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+      },
+    ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/github',
+      {
+        successComment: false,
+        // Create a draft release for manual approval on push to 'main'.
+        draftRelease: process.env.BRANCH_NAME === 'main',
+      },
+    ],
+  ],
+};
